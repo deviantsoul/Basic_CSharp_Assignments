eb1487026b1595f970ad11736f04c725f2ab7ccc
hansenu <ucabhansen@hotmail.com>
2021-02-04 15:04:19 -0500

    Finally

--- Boolean Logic/Boolean Logic/Program.cs
+++ Boolean Logic/Boolean Logic/Program.cs
@@ -12,13 +12,13 @@ namespace Boolean_Logic
             int age = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
             Console.WriteLine("Have you ever had a DUI? Please answer Yes or No.");
+            string input = Console.ReadLine();
+            bool DUI = input == "No";
             Console.ReadLine();
-            bool Yes = true;
-            bool No = false;
             Console.WriteLine("How many speeding tickets do you have?");
             int ticket = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
-            bool Qualified = (age > 15) && Yes && (ticket <= 3);
+            bool Qualified = (age > 15) && DUI && (ticket <= 3);
             Console.WriteLine("Qualified? - " + Qualified);
             Console.ReadLine();
         }
