f4564659cdca852b3df76f90bad69165eaa90a14
hansenu <ucabhansen@hotmail.com>
2021-04-09 15:25:27 -0400

    Lambda Expression Assignment
    1. In the Main() method, create a list of at least 10 employees. Each employee should have a first and last name, as well as an Id. At least two employees should have the first name "Joe".
    2. Using a foreach loop, create a new list of all employees with the first name "Joe".
    3. Perform the same action again, but this time with a lambda expression.
    4. Using a lambda expression, make a list of all employees with an Id number greater than 5.

---
+++ Lambda Expression Assignment/Lambda Expression Assignment/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expression_Assignment
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        public Employee(string _FirstName, string _LastName, int _Id)
+        {
+            FirstName = _FirstName;
+            LastName = _LastName;
+            Id = _Id;
+        }
+    }
+}
---
+++ Lambda Expression Assignment/Lambda Expression Assignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expression_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> Employees = new List<Employee>();
+            List<string> FirstNames = new List<string>()
+            {
+                "Paisely", "Laura", "Ivy", "Stephanie", "Jean", "Maui", "Crystal", "Joe", "Joe", "Dua"
+            };
+
+            List<string> LastNames = new List<string>()
+            {
+                "Smith", "Goddess", "Sy", "Lee", "Sexy", "Taylor", "Mana", "Amen", "Amazing", "Lipa"
+            };
+
+            List<int> IDs = new List<int>()
+            {
+                1, 6, 2, 3, 7, 9, 8, 4, 5, 12
+            };
+
+            int i = 0;
+            foreach (string firstName in FirstNames)
+            {
+                Employees.Add(new Employee(firstName, LastNames[i], IDs[i]));
+                i++;
+            }                      
+
+            List<Employee> joeList = Employees.Where( x => x.FirstName.Contains("Joe")).ToList();
+
+            foreach (Employee joes in joeList)
+            {
+                Console.WriteLine(joes.FirstName);
+            }
+            Console.ReadLine();
+
+            List<Employee> moreThanFive = Employees.Where(y => y.Id > 5).ToList();
+
+            foreach (Employee myFives in moreThanFive)
+            {
+                Console.WriteLine(myFives.Id);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
